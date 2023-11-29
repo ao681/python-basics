e1dc32f63caec10b4cac379085351c65c0530a6f
ao681 <84420194+ao681@users.noreply.github.com>
2023-11-29 13:19:56 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+
+setup(
+    name='taskaty',
+    version='0.1.0',
+    description='A simple command-line Task-app written in Python',
+    author='Mohammed Taher',
+    install_requires=['tabulate'],
+    entry_points={
+        'console_scripts': [
+            'taskaty=taskaty:main',
+        ],
+    },
+)
